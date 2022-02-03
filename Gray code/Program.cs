27a6ad275c70f768b75890527c61a723ffa0bd24
roystondsouz <98145297+roystondsouz@users.noreply.github.com>
2022-02-03 13:00:03 +0530

    Add files via upload

---
+++ Gray code/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace prog4
+{
+    class GrayCode
+    {
+        static int getGray(int n)
+        {
+            return n ^ (n >> 1);
+        }
+        static void Main(string[] args)
+        {
+            int InputNum, GrayNum;
+            Console.WriteLine("\n enter the decimal number:");
+            InputNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\nBinary equivalent of {0}:{1}", InputNum, Convert.ToString(InputNum, 2));
+            GrayNum = getGray(InputNum);
+            Console.WriteLine("\ngray code equivalent of {0}:{1}", InputNum, Convert.ToString(GrayNum, 2));
+        }
+    }
+}
