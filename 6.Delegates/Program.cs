ce0a8d81862e5fa470fc025db14fa91fe8709bea
KING <98145297+roystondsouz@users.noreply.github.com>
2022-02-08 12:56:31 +0530

    Add files via upload

---
+++ 6.Delegates/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exercises
+{
+    class Delegates
+    {
+        delegate string UppercaseDelegate(string input);
+        static string UppercaseFirst(string input)
+        {
+            char[] buffer = input.ToCharArray();
+            buffer[0] = char.ToUpper(buffer[0]);
+            return new string(buffer);
+        }
+        static string UppercaseLast(string input)
+        {
+            char[] buffer = input.ToCharArray();
+            buffer[buffer.Length - 1] = char.ToUpper(buffer[buffer.Length - 1]);
+            return new string(buffer);
+        }
+        static string UppercaseAll(string input)
+        {
+            return input.ToUpper();
+        }
+        static void WriteOutput(string input, UppercaseDelegate del)
+        {
+            Console.WriteLine("Input String: {0}", input);
+            Console.WriteLine("Output String: {0}", del(input));
+        }
+        static void Main()
+        {
+            WriteOutput("tom", new UppercaseDelegate(UppercaseFirst));
+            WriteOutput("tom", new UppercaseDelegate(UppercaseLast));
+            WriteOutput("tom", new UppercaseDelegate(UppercaseAll));
+            Console.ReadLine();
+        }
+    }
+}
