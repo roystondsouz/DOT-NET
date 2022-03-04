87bb8caadc51df39ee77bf3f45c5b632aa80d248
KING <98145297+roystondsouz@users.noreply.github.com>
2022-03-04 12:51:36 +0530

    Add files via upload

---
+++ hello word/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hello_word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
