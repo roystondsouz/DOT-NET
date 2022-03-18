85e12bb9ededc6f84cdd8b908c479d238b874414
KING <98145297+roystondsouz@users.noreply.github.com>
2022-03-18 12:19:52 +0530

    Add files via upload

---
+++ 07.Register number/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace Exercises
+{
+    class Register_number
+    {
+        int regno;
+        static int startnum;
+        static Register_number()
+        {
+            startnum = 20210000;
+        }
+        Register_number()
+        {
+            regno = ++startnum;
+        }
+        public static void Main(string[] args)
+
+        {
+            for (int i = 0; i < 100; i++)
+            {
+
+                Register_number student = new Register_number();
+                Console.WriteLine("students {0} : {1}",i+1,student.regno);
+            }
+
+        }
+    }
+}
+
+
+
