a6f0418c9dbdec380aa2aa17d7c3e5a3eadb620d
KING <98145297+roystondsouz@users.noreply.github.com>
2022-04-01 11:50:27 +0530

    Add files via upload

---
+++ 10.SumOfDiagonals/11.Existence/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+namespace Exercises
+{
+    class FileRead
+    {
+        public static void Main()
+        {
+            string fileName;
+            while (true)
+            {
+                Console.WriteLine("\n --------------- MENU --------------- \n");
+                Console.WriteLine("\n 1.Create a File ");
+                Console.WriteLine("\n 2.Existence of the File ");
+                Console.WriteLine("\n 3.Read the contents of the File ");
+                Console.WriteLine("\n 4.Exit ");
+                Console.Write("\n Enter your choice : ");
+                int ch = int.Parse(Console.ReadLine());
+                switch (ch)
+                {
+                    case 1:
+                        Console.Write("\n Enter the file name to create: ");
+                        fileName = Console.ReadLine();
+                        Console.WriteLine("\n Write the Contents to the File: \n");
+                        string r = Console.ReadLine();
+                        using (StreamWriter fileStr = File.CreateText(fileName))
+                        {
+                            fileStr.WriteLine(r);
+                        }
+                        Console.WriteLine("File is Created…");
+                        break;
+                    case 2:
+                        Console.Write("\n Enter the file name:");
+                        fileName = Console.ReadLine();
+                        if (File.Exists(fileName))
+                        {
+                            Console.WriteLine("File exists...");
+                        }
+                        else
+                        {
+                            Console.WriteLine("File does not exist in the current directory!");
+                        }
+                        break;
+                    case 3:
+                        Console.Write("Enter the file name to read the contents:\n");
+                        fileName = Console.ReadLine();
+                        if (File.Exists(fileName))
+                        {
+                            using (StreamReader sr = File.OpenText(fileName))
+                            {
+                                string s = "";
+                                Console.WriteLine(" Here is the content of the file : ");
+                                while ((s = sr.ReadLine()) != null)
+                                {
+                                    Console.WriteLine(s);
+                                }
+                                Console.WriteLine("");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("File does not exists");
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine("\n Exiting...");
+                        return;
+                    default:
+                        Console.WriteLine("\n Invalid choice");
+                        break;
+                }
+            }
+        }
+    }
+}
