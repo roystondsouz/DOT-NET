d9aa3671ec5c46fea5436087c7d19cf93abd86c7
KING <98145297+roystondsouz@users.noreply.github.com>
2022-04-01 11:53:01 +0530

    Add files via upload

---
+++ 12.File Comparison/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+namespace Exercises
+{
+    class FileRead
+    {
+        public static void Main()
+        {
+            string file1;
+            string file2;
+
+            Console.Write("Enter the first file path:");
+            file1 = Console.ReadLine();
+            Console.Write("Enter the second file path:");
+            file2 = Console.ReadLine();
+            if (!File.Exists(file1))
+            {
+                Console.WriteLine("First file does not exist!");
+            }
+            else if (!File.Exists(file2))
+            {
+                Console.WriteLine("Second file does not exist!");
+            }
+            else if (File.ReadAllText(file1) == File.ReadAllText(file2))
+            {
+                Console.WriteLine("Both files contain the same content");
+            }
+            else
+            {
+                Console.WriteLine("Contents of files are not same");
+            }
+        }
+    }
+}
